4b09a06db5f825771731ec728c98d76b9a7e6618
Abdul Wahab <riazw020@gmail.com>
2021-01-15 15:01:21 +0500

    JavaScript Chapter 1-30

---
+++ app.js
@@ -0,0 +1,108 @@
+
+// ********** | Changing case | **********
+
+// var a = prompt("Enter your name")
+// var a = a.toUpperCase()
+// console.log(a)
+
+// var a = prompt("Enter your name")
+// var a = a.toLowerCase()
+// console.log(a)
+
+
+// ********** | Strings: measuring length and extracting parts | **********
+
+// var a = prompt("Enter your name")
+// console.log(a)
+// document.write("YOUR WORD'S LENGHT IS:  " + a.length)
+
+
+// ********** | Strings: finding segments| **********
+
+// var a = "pakistan"
+// var b = a.indexOf("n")
+// console.log(b)
+
+// var a = prompt("Enter your name")
+// var b = a.indexOf("@", ".", ",", "!")
+// if (b > -1) {
+//     alert("Enter a valid username")
+// }
+
+// var a = "The quick brown fox jumps over the lazy dog"
+// var b = a.indexOf("the")
+// console.log(b)
+
+
+// ********** | Strings: finding a character at a location | **********
+
+// var a = "pakistan"
+// var b = a.charAt(3)
+// console.log(b)
+
+
+// ********** | Strings: replacing characters | **********
+
+// var a = "Hyderabad"
+// var b = a.replace("Hyder", "Islam")
+// console.log (b)
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var b = message.replace(/and/g, "&")
+// console.log(b)
+
+
+// ********** | Rounding numbers | **********
+
+// var a =  3.45214
+// var b = Math.round(a)
+// console.log(b)
+// var c = Math.floor(a)
+// console.log(c)
+// var d = Math.ceil(a)
+// console.log(d)
+
+// var a =  -2.678
+// var b = Math.round(a)
+// console.log(b)
+// var c = Math.floor(a)
+// console.log(c)
+// var d = Math.ceil(a)
+// console.log(d)
+
+
+// ********** | Generating random numbers | **********
+
+// var a = Math.random() * 6
+// var a = Math.ceil(a)
+// console.log(a)
+
+// var player1 = prompt("Player 1 (Heads/Tails)")
+// var player2 = prompt("Player 2 (Heads/Tails)")
+// var toss = Math.random() * 2
+// if (toss > 1) {
+//     alert("Heads")
+// } else {
+//     alert("Tails")
+// }
+
+
+// ********** | Converting strings to numbers, numbers to strings | **********
+
+// var a = "472"
+// var b = Number(a)
+// console.log(typeof(b), b)
+
+// var a = 3536;
+// var b = a.toString()
+// console.log(typeof(b), b)
+
+
+// ********** | Controlling the length of decimals | **********
+
+// var percentage = 30 / 45 * 100
+// console.log(percentage.toFixed(2))
+
+
+
+
